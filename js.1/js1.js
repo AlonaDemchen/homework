0b5505fe1e302e563dee25b7f8ed8d2fba049a15
username <usermail>
2023-05-17 23:04:21 +0300

    Додано новий файл

---
+++ js.1/js1.js
@@ -0,0 +1,10 @@
+let firstNumber = parseFloat(prompt("Введите первое значение:")); //вводим первое число
+
+let secondNumber = parseFloat(prompt("Введите второе значение:")); //вводим второе число
+
+let partialResult1 = firstNumber * 2; //первое число умнажаем на 2
+let partialResult2 = secondNumber / 2; //второе число делим на 2
+
+let result = partialResult1 + partialResult2; //вычисление результата
+
+alert("Результат: " + result); // выводим результат на экран
